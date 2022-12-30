364e0e49a3c1d03a042a7145450f9a21a307e47f
jeong jun <jungjunjenon3047@gmail.com>
2022-12-30 14:35:24 +0900

    add <Provider>

--- InhaClub/front/src/index.tsx
+++ InhaClub/front/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const rootElement = document.getElementById('root');
 if(!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
