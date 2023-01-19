a941fb8c64e177d3c999e9a30f54260b3850e1b9
jeong jun <jungjunjenon3047@gmail.com>
2023-01-19 12:48:28 +0900

    make code clean

--- InhaClub/front/src/component/Header.tsx
+++ InhaClub/front/src/component/Header.tsx
@@ -1,21 +1,18 @@
-import e from "express";
-import { ReactComponentElement, useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 import { addKeyword, setIsSearch, subKeyword } from '../store/store'
 
 
-const keywordList = ['운동','달리기', '운동동아리', '운동하는남자', '요리조리피하기', '헬스']
+const keywordList = ['운동', '달리기', '운동동아리', '운동하는남자', '요리조리피하기', '헬스', '대형', '소형', '중앙','연합','인기','신규','종교','예술', '음악', '학술']
 
 const Header = ():any => {
 
     const navigate = useNavigate()
-    // const [keyword, setKeyword]: any = useState([])
     const dispatch = useDispatch()
     const keyword = useSelector((state:any) => state.keyword)
 
     const [nowKeywordList, setNowKeywordList] = useState(['null'])
-
     return<>
     <header>
         <div>
@@ -29,6 +26,7 @@ const Header = ():any => {
             </div>
             <div className="header-bottom">
                 <div className="search">
+
                     {
                         keyword.map((a:any,i:any)=>{
                             return<div className="keyword" key={i}>
@@ -51,16 +49,12 @@ const Header = ():any => {
                         if(nowKeyword === ""){
                             setNowKeywordList(['null'])
                         }else{
-                            console.log("nowKeyword: "+ nowKeyword);
-                            console.log("keyword: "+ keyword);
-                            
                             let temp = keywordList.filter(key => key.startsWith(nowKeyword) && !keyword.includes(key))
                             setNowKeywordList(temp)
                         }
 
                         if(nowKeyword.endsWith(" ")){
                             nowKeyword = nowKeyword.trim();
-                            console.log(nowKeyword);
                             e.target.value = "";
 
                             if(nowKeyword != " " && nowKeywordList.includes(nowKeyword)){
@@ -70,7 +64,7 @@ const Header = ():any => {
                         }} placeholder="ex) 음악, 운동, 학술"
                         />
                 </div>
-                <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" 
+                <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"
                 onClick={()=>{
                     dispatch(setIsSearch(true))
                 }}/>
